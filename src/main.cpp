9b171aee93819a841705861cc905aa18756fd1e0
hktkzyx <hktkzyx@yeah.net>
2022-03-08 20:45:56 +0800

    feat: use advertising to publish measured data
    BREAKING CHANGE: use advertising instead of connection to publish measured data

--- src/main.cpp
+++ src/main.cpp
@@ -14,112 +14,78 @@
 #include "config.h"
 #include "misc.h"
 
+#define COUNT_OF(x) \
+    ((sizeof(x) / sizeof(0 [x])) / ((size_t)(!(sizeof(x) % sizeof(0 [x])))))
 #define I2C_SDA 4
 #define I2C_SCL 3
 
-const BLEUUID kEnvironmentalSensorServiceUUID =
-    BLEUUID(static_cast<uint16_t>(0x181A));
-const BLEUUID kTemperatureUUID = BLEUUID(static_cast<uint16_t>(0x2A6E));
-const BLEUUID kHumidityUUID = BLEUUID(static_cast<uint16_t>(0x2A6F));
-const BLEUUID kIlluminanceUUID = BLEUUID(static_cast<uint16_t>(0x2AFB));
-const uint16_t kTemperatureDefault = 0x8000;
-const uint16_t kHumidityDefault = 0xFFFF;
-const uint8_t kIlluminanceDefault[3] = {0xFF, 0xFF, 0xFF};
 const uint8_t kSHTAddress = 0x44;
 const uint8_t kLightAddress = 0x23;
 
-SHTSensor sht(SHTSensor::SHT3X);
-BH1750 light_meter(kLightAddress);
-BLECharacteristic *pTemperature = nullptr;
-BLECharacteristic *pHumidity = nullptr;
-BLECharacteristic *pIlluminance = nullptr;
-bool connect = false;
-uint32_t status_change = 0;
+float temperature_buffer = -300;
+float humidity_buffer = -1;
+float illuminance_buffer = -1;
 
-class MyServerCallbacks : public BLEServerCallbacks {
-    void onConnect(BLEServer *pServer) {
-        Serial.println("A device connected!");
-        connect = true;
-        status_change = millis();
+std::string GenerateServiceData(const float &temperature, const float &humidity,
+                                const float &illuminance) {
+    uint16_t temperature_data = 0x8000;
+    if (temperature <= 327.67 && temperature >= -273.15) {
+        temperature_data = round(temperature * 100);
     }
+    uint16_t humidity_data = 0xFFFF;
+    if (humidity >= 0 && humidity <= 100) {
+        humidity_data = round(humidity * 100);
+    }
+    uint32_t illuminance_data = 0x00FFFFFF;
+    if (illuminance >= 0 && illuminance <= 167772.14) {
+        illuminance_data = round(illuminance * 100);
+    }
+    return std::string(reinterpret_cast<char *>(&temperature_data), 2) +
+           std::string(reinterpret_cast<char *>(&humidity_data), 2) +
+           std::string(reinterpret_cast<char *>(&illuminance_data), 3);
+}
 
-    void onDisconnect(BLEServer *pServer) {
-        Serial.println("A device is disconnected!");
-        connect = false;
-        status_change = millis();
-        Serial.println("Going to sleep now because disconnect.");
-        esp_deep_sleep_start();
-    }
-};
+std::string GenerateServiceData() { return GenerateServiceData(-300, -1, -1); }
 
-class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
-    void onRead(BLECharacteristic *pCharacteristic,
-                esp_ble_gatts_cb_param_t *param) {
-        Serial.println("A device read the data.");
-    }
-    void onStatus(BLECharacteristic *pCharacteristic, Status s, uint32_t code) {
-        if (s == BLECharacteristicCallbacks::SUCCESS_INDICATE) {
-            Serial.println("A device has received the data.");
+BLEAdvertisementData GenerateAdvertisedData(uint8_t *pManufacturer,
+                                            const size_t manufacturer_size,
+                                            char *pName, const size_t name_size,
+                                            BLEUUID &service_uuid,
+                                            std::string &service_data) {
+    BLEAdvertisementData data;
+    data.setFlags(ESP_BLE_ADV_FLAG_GEN_DISC);
+    std::string manufacturer(reinterpret_cast<char *>(pManufacturer),
+                             manufacturer_size);
+    data.setManufacturerData(manufacturer);
+    std::string name(pName, name_size);
+    data.setName(name);
+    data.setServiceData(service_uuid, service_data);
+    return data;
+}
+
+void Publish(const uint32_t millis_interval, const uint32_t millis_duration) {
+    static uint32_t last_publish = -1;
+    uint32_t now = millis();
+    if (static_cast<uint32_t>(now - last_publish) > millis_interval ||
+        last_publish == -1) {
+        last_publish = now;
+        uint8_t manufacturer_id[3] = {0xE5, 0x02, 0x66};
+        char device_name[] = "sensor01";
+        BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+        BLEUUID service_uuid(static_cast<uint16_t>(0xFC00));
+        std::string service_data = GenerateServiceData(
+            temperature_buffer, humidity_buffer, illuminance_buffer);
+        BLEAdvertisementData data = GenerateAdvertisedData(
+            manufacturer_id, 3, device_name, COUNT_OF(device_name),
+            service_uuid, service_data);
+        pAdvertising->setAdvertisementData(data);
+        BLEDevice::startAdvertising();
+        uint32_t start = millis();
+        while (static_cast<uint32_t>(millis() - start) <= millis_duration) {
+            delay(100);
         }
+        BLEDevice::stopAdvertising();
     }
-};
-
-BLECharacteristic *CharacteristicSetup(BLEService *pService, BLEUUID uuid,
-                                       uint16_t default_value) {
-    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-        uuid, BLECharacteristic::PROPERTY_READ |
-                  BLECharacteristic::PROPERTY_INDICATE);
-    pCharacteristic->addDescriptor(new BLE2902());
-    pCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
-    pCharacteristic->setValue(default_value);
-    return pCharacteristic;
-}
-
-BLECharacteristic *CharacteristicSetup(BLEService *pService, BLEUUID uuid,
-                                       uint8_t *pDefaultValue, size_t length) {
-    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-        uuid, BLECharacteristic::PROPERTY_READ |
-                  BLECharacteristic::PROPERTY_INDICATE);
-    pCharacteristic->addDescriptor(new BLE2902());
-    pCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
-    pCharacteristic->setValue(pDefaultValue, length);
-    return pCharacteristic;
-}
-
-BLECharacteristic *CharacteristicSetup(BLEService *pService, BLEUUID uuid,
-                                       const uint8_t *pDefaultValue,
-                                       const size_t length) {
-    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-        uuid, BLECharacteristic::PROPERTY_READ |
-                  BLECharacteristic::PROPERTY_INDICATE);
-    pCharacteristic->addDescriptor(new BLE2902());
-    pCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
-    uint8_t pValue[length] = {0};
-    for (int i = 0; i < length; ++i) {
-        pValue[i] = pDefaultValue[i];
-    }
-    pCharacteristic->setValue(pValue, length);
-    return pCharacteristic;
-}
-void BluetoothSetup() {
-    Serial.println("Start setup BLE.");
-    BLEDevice::init("ESP32BLE");
-    BLEServer *pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new MyServerCallbacks());
-    BLEService *pService =
-        pServer->createService(kEnvironmentalSensorServiceUUID);
-    pTemperature =
-        CharacteristicSetup(pService, kTemperatureUUID, kTemperatureDefault);
-    pHumidity = CharacteristicSetup(pService, kHumidityUUID, kHumidityDefault);
-    pIlluminance =
-        CharacteristicSetup(pService, kIlluminanceUUID, kIlluminanceDefault, 3);
-    pService->start();
-    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-    pAdvertising->setScanResponse(true);
-    pAdvertising->setMinPreferred(
-        0x06);  // functions that help with iPhone connections issue
-    pAdvertising->setMinPreferred(0x12);
-    BLEDevice::startAdvertising();
 }
 
 void DeepSleepSetup(uint16_t seconds) {
@@ -150,29 +116,14 @@ void ScanI2CDevices() {
     }
 }
 
-void SensorsSetup() {
-    Wire.begin(I2C_SDA, I2C_SCL, 100000);
-    // ScanI2CDevices();
-    sht.setAccuracy(SHTSensor::SHT_ACCURACY_HIGH);
-    if (sht.init()) {
-        Serial.println("SHT Sensor init success.");
-    } else {
-        Serial.println("SHT Sensor init fail.");
-    }
-    if (light_meter.begin(BH1750::ONE_TIME_HIGH_RES_MODE)) {
-        Serial.println("BH1750 init success.");
-    } else {
-        Serial.println("BH1750 init fail.");
-    }
-}
-
 void setup() {
     esp_task_wdt_init(IDLE_LIMIT, true);
     esp_task_wdt_add(NULL);
     Serial.begin(115200);
-    SensorsSetup();
-    BluetoothSetup();
-    DeepSleepSetup(WAKEUP_INTERVAL);
+    // ScanI2CDevices();
+    Wire.begin(I2C_SDA, I2C_SCL, 100000);
+    BLEDevice::init("ESP32 BLE Sensor");
+    // DeepSleepSetup(WAKEUP_INTERVAL);
     Serial.println("ESP32BLE setup done!");
 }
 
@@ -186,105 +137,59 @@ void WatchdogReset(const uint32_t interval) {
     }
 }
 
-void UpdateSHT(float &temperature, bool &temperature_valid, float &humidity,
-               bool &humidity_valid) {
+void UpdateSHT(float &temperature, float &humidity) {
+    SHTSensor sht(SHTSensor::SHT3X);
+    sht.setAccuracy(SHTSensor::SHT_ACCURACY_HIGH);
+    if (sht.init()) {
+        Serial.println("SHT Sensor init success.");
+    } else {
+        Serial.println("SHT Sensor init fail.");
+    }
     if (sht.readSample()) {
         temperature = sht.getTemperature();
-        temperature_valid = true;
         humidity = sht.getHumidity();
-        humidity_valid = true;
         Serial.printf("Temperature is %.2f °C, humidity is %.2f%", temperature,
                       humidity);
         Serial.println();
     } else {
-        temperature_valid = false;
-        humidity_valid = false;
+        temperature = -300;
+        humidity = -1;
     }
 }
 
-void UpdateBH1750(float &illuminance, bool &illuminance_valid) {
-    light_meter.configure(BH1750::ONE_TIME_HIGH_RES_MODE);
+void UpdateBH1750(float &illuminance) {
+    BH1750 light_meter(kLightAddress);
+    if (light_meter.begin(BH1750::ONE_TIME_HIGH_RES_MODE)) {
+        Serial.println("BH1750 init success.");
+    } else {
+        Serial.println("BH1750 init fail.");
+    }
     while (!light_meter.measurementReady(true)) {
         delay(120);
     }
     float lux = light_meter.readLightLevel();
     if ((lux == -1) | (lux == -2)) {
-        illuminance_valid = false;
+        illuminance = -1;
     } else {
         illuminance = lux;
-        illuminance_valid = true;
         Serial.printf("Illuminance is %.2f lux", illuminance);
         Serial.println();
     }
 }
 
-void PublishTemperature(BLECharacteristic *pCharacteristic, float temperature,
-                        bool valid = true) {
-    uint16_t value = kTemperatureDefault;
-    if (valid) {
-        value = round(temperature * 100);
+void MeasureCycle(const uint32_t interval_millis) {
+    static uint32_t last_measure = -1;
+    uint32_t now = millis();
+    if (static_cast<uint32_t>(now - last_measure) > interval_millis ||
+        last_measure == -1) {
+        last_measure = now;
+        UpdateSHT(temperature_buffer, humidity_buffer);
+        UpdateBH1750(illuminance_buffer);
     }
-    pCharacteristic->setValue(value);
-    Serial.print("Temperature is update to ");
-    Serial.print(value);
-    Serial.println();
-    pCharacteristic->indicate();
-}
-
-void PublishHumidity(BLECharacteristic *pCharacteristic, float humidity,
-                     bool valid = true) {
-    uint16_t value = kHumidityDefault;
-    if (valid) {
-        value = round(humidity * 100);
-    }
-    pCharacteristic->setValue(value);
-    Serial.print("Humidity is update to ");
-    Serial.print(value);
-    Serial.println();
-    pCharacteristic->indicate();
-}
-
-void PublishIlluminance(BLECharacteristic *pCharacteristic, float illuminance,
-                        bool valid = true) {
-    uint8_t pValue[3] = {0};
-    for (int i = 0; i < 3; ++i) {
-        pValue[i] = kIlluminanceDefault[i];
-    }
-    if (valid) {
-        uint32_t value = round(illuminance * 100);
-        pValue[0] = value;
-        pValue[1] = value >> 8;
-        pValue[2] = value >> 16;
-    }
-    pCharacteristic->setValue(pValue, 3);
-    Serial.printf("Illuminace is update to 0x%x-%x-%x", pValue[0], pValue[1],
-                  pValue[2]);
-    Serial.println();
-    pCharacteristic->indicate();
 }
 
 void loop() {
-    WatchdogReset(1000 * UPDATE_INTERVAL);
-    uint32_t now = millis();
-    // connect = true;  // For debug to disable deep sleep
-    if ((!connect) &&
-        (static_cast<uint32_t>(now - status_change) > IDLE_LIMIT * 1000U)) {
-        Serial.println("Going to sleep now because idle");
-        esp_deep_sleep_start();
-    }
-    if (connect) {
-        static uint32_t last_update = 0;
-        if ((static_cast<uint32_t>(now - last_update) >
-             UPDATE_INTERVAL * 1000U) ||
-            (last_update == 0)) {
-            float temperature, humidity, illuminace = 0;
-            bool temperature_valid, humidity_valid, illuminace_valid = false;
-            UpdateSHT(temperature, temperature_valid, humidity, humidity_valid);
-            UpdateBH1750(illuminace, illuminace_valid);
-            PublishTemperature(pTemperature, temperature, temperature_valid);
-            PublishHumidity(pHumidity, humidity, humidity_valid);
-            PublishIlluminance(pIlluminance, illuminace, illuminace_valid);
-            last_update = now;
-        }
-    }
+    WatchdogReset(1000 * PUBLISH_INTERVAL);
+    MeasureCycle(1000 * MEASURE_INTERVAL);
+    Publish(1000 * PUBLISH_INTERVAL, 1000 * PUBLISH_DURATION);
 }
